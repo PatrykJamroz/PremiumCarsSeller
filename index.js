2da648c87fcaf27d7718340e33febb1119ae5625
PJ <jamroz.patryk@gmail.com>
2020-10-14 20:47:31 +0200

    added some styles

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 const slides = document.getElementsByClassName("carousel-item");
 //console.log(slides[0])
-let slidePosition = 0
+let slidePosition = 0;
 const totalSlides = slides.length;
-console.log(totalSlides)
-
-
+console.log(totalSlides);
 
 function hideAllSlides() {
-    for (let slide of slides) {
-        slide.classList.remove("carousel-item-visible")
-        slide.classList.add("carousel-item-hidden")
-    }
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
 }
 
 function nextSlide() {
-    
-    hideAllSlides()
-    
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
+  hideAllSlides();
 
-    slides[slidePosition].classList.add("carousel-item-visible")
-    //console.log(slides[slidePosition].classList)
-    //console.log(slidePosition)
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  slides[slidePosition].classList.add("carousel-item-visible");
+  //console.log(slides[slidePosition].classList)
+  //console.log(slidePosition)
 }
-
 
 function changeSlide() {
-    setInterval(nextSlide, 5000)
+  setInterval(nextSlide, 5000);
 }
 
-changeSlide()
+changeSlide();
